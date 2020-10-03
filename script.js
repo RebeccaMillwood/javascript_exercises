8ce6b5e71dbaa09904a5ed85ffeffe1659280166
RebeccaMillwood <bec-millwood@live.com.au>
2020-10-03 12:58:45 +1000

    added function to add to cart - exercise 2

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function decrement(){
     value--;
     document.getElementById("number").value = value;
 }
+
+
+function cart(cupcake){
+    // var value = parseInt(document.getElementById("cart").value, 10);
+    // value = isNaN(value) ? 0 : value;
+    // value++;
+    // document.getElementById("cart").value = value;
+    var value = document.getElementById("cart").value;
+    value = value + "\n" + cupcake;
+    document.getElementById("cart").value = value;
+    document.getElementById("cart").rows++;
+}
