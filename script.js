454519201f57f67bf0efcbc62236fee97b7e279e
RebeccaMillwood <bec-millwood@live.com.au>
2020-10-03 10:44:36 +1000

    updated ID - number function working now!

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ function increment(){
     value++;
     document.getElementById("number").value = value;
 }
+
+function decrement(){
+    var value = parseInt(document.getElementById("number").value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById("number").value = value;
+}
